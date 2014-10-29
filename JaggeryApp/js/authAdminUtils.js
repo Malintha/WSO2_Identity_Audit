ec267bb06f71fd22f1f21d904a2b7eef2328f27b
Malintha <malinthak2@gmail.com>
2014-10-29 15:32:47 +0530

    added Pie

--- JaggeryApp/js/authAdminUtils.js
+++ JaggeryApp/js/authAdminUtils.js
@@ -1,9 +1,10 @@
-var tableData =null;
+var tableData = null;
 var isFirstSearch = true;
 
 $(document).ready(function () {
-    loadDefaultDatafromDB(function(resultArray) {
+    loadDefaultDatafromDB(function (resultArray) {
         populateTable(resultArray);
+        generatePieData();
         console.log('done');
     });
     $('.input-daterange').datepicker({
@@ -20,7 +21,7 @@ function loadDefaultDatafromDB(cb) {
 
 function populateTable(resultArray) {
     tableData = $('#example').on('search.dt', function () {
-        if(isFirstSearch){
+        if (isFirstSearch) {
             console.log('first search');
             isFirstSearch = false;
         }
@@ -42,8 +43,55 @@ function populateTable(resultArray) {
     });
 }
 
-    var searchEventFired = function () {
-        console.log(tableData.fnSettings().fnRecordsDisplay());
-    }
+var searchEventFired = function () {
+    console.log(tableData.fnSettings().fnRecordsDisplay());
+}
 
+function displayGraph() {
+    nv.addGraph(function () {
+        var width = 300,
+            height = 300;
+
+        var chart = nv.models.pieChart()
+            .x(function (d) {
+                return d.key
+            })
+            .y(function (d) {
+                return d.y
+            })
+            .color(d3.scale.category10().range())
+            .width(width)
+            .height(height);
+
+        d3.select("#test1")
+            .datum(dataarr)
+            .transition().duration(1200)
+            .attr('width', width)
+            .attr('height', height)
+            .call(chart);
+
+        chart.dispatch.on('stateChange', function (e) {
+            nv.log('New State:', JSON.stringify(e));
+        });
+
+        return chart;
+    });
+}
+
+function pieData(key, y) {
+    this.key = key;
+    this.y = y;
+}
+
+var dataarr = [];
+
+function generatePieData() {
+    var pd1 = new pieData("True", 14);
+    var pd2 = new pieData("False", 19);
+    dataarr.push(pd1);
+    dataarr.push(pd2);
+
+    console.log(dataarr[0]);
+    displayGraph();
+}
 
