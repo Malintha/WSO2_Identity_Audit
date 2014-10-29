7a2d1f35850a66686b724298156b9a8ee5a67874
Malintha <malinthak2@gmail.com>
2014-10-29 13:00:22 +0530

    callbacks everywhere :D

--- JaggeryApp/js/authAdminUtils.js
+++ JaggeryApp/js/authAdminUtils.js
@@ -1,36 +1,49 @@
-var resarr;
+var tableData =null;
+var isFirstSearch = true;
 
-function clickgetdata() {
-    console.log('btn clicked');
-    $.get("service.jag", null, function(data){
-        // populateTable(data);
-        resarr = JSON.parse(data);
-        test();
+$(document).ready(function () {
+    loadDefaultDatafromDB(function(resultArray) {
+        populateTable(resultArray);
+        console.log('done');
     });
-}
-
-function test() {
-    populateTable();
-}
-
-function populateTable() {
-    $('#example').dataTable( {
-    	"bProcessing": true,
-        "aaData": resarr,
-        "paging":   true,
-        "ordering": true,
-        "info":     true,
-        "aoColumns": [
-            { "mData": "Timestampstr", "class":"center"},
-            { "mData": "UserName","class":"center" },
-            { "mData": "RemoteHost","class":"center" },
-            { "mData": "Response","class":"center" }
-        ]
-    } );
-}
-
-$(document).ready(function() {
     $('.input-daterange').datepicker({
         todayBtn: "linked"
     });
 });
+
+function loadDefaultDatafromDB(cb) {
+    $.get("service.jag", null, function (data) {
+        resultArray = JSON.parse(data);
+        cb(resultArray);
+    });
+}
+
+function populateTable(resultArray) {
+    tableData = $('#example').on('search.dt', function () {
+        if(isFirstSearch){
+            console.log('first search');
+            isFirstSearch = false;
+        }
+        else {
+            searchEventFired();
+        }
+    }).dataTable({
+        "bProcessing": true,
+        "aaData": resultArray,
+        "paging": true,
+        "ordering": true,
+        "info": true,
+        "aoColumns": [
+            { "mData": "Timestampstr", "class": "center"},
+            { "mData": "UserName", "class": "center" },
+            { "mData": "RemoteHost", "class": "center" },
+            { "mData": "Response", "class": "center" }
+        ]
+    });
+}
+
+    var searchEventFired = function () {
+        console.log(tableData.fnSettings().fnRecordsDisplay());
+    }
+
+
