da02329a3f8f05a71aa45130f10b894ed5c76de3
Malintha <malinthak2@gmail.com>
2014-10-29 17:44:50 +0530

    datatables and pie connected

--- JaggeryApp/js/authAdminUtils.js
+++ JaggeryApp/js/authAdminUtils.js
@@ -1,10 +1,13 @@
 var tableData = null;
 var isFirstSearch = true;
+//var pieData = null;
 
 $(document).ready(function () {
     loadDefaultDatafromDB(function (resultArray) {
         populateTable(resultArray);
-        generatePieData();
+        var a = getStatsFromTable();
+        console.log(a[0]+","+a[1]);
+        generatePie(a[0],a[1]);
         console.log('done');
     });
     $('.input-daterange').datepicker({
@@ -44,11 +47,74 @@ function populateTable(resultArray) {
 }
 
 var searchEventFired = function () {
-    console.log(tableData.fnSettings().fnRecordsDisplay());
+   var r = getStatsFromTable();
+   var temp = [];
+    console.log(r[0]+" , "+r[1]);
+
+    var pd1 = new pieData("True", r[0]);
+    var pd2 = new pieData("False", r[1]);
+
+    temp.push(pd1);
+    temp.push(pd2);
+
+    d3.selectAll("svg > *").remove();
+
+    var pieChart = nv.addGraph(function () {
+
+        var width = 300,
+            height = 300;
+
+        var chart = nv.models.pieChart()
+            .x(function (d) {
+                return d.key
+            })
+            .y(function (d) {
+                return d.y
+            })
+            .color(d3.scale.category10().range())
+            .width(width)
+            .height(height);
+
+        d3.select("#test1")
+            .datum(temp)
+            .transition().duration(100)
+            .attr('width', width)
+            .attr('height', height)
+            .call(chart);
+
+        chart.dispatch.on('stateChange', function (e) {
+            nv.log('New State:', JSON.stringify(e));
+        });
+
+        return chart;
+    });
+   // generatePie(r[0],r[1]);
+}
+
+function getStatsFromTable() {
+//    var length = tableData.fnSettings().fnRecordsDisplay();
+    var filteredrows = tableData._('tr', {"filter": "applied"});
+
+    var trueCount = 0;
+    var falseCount = 0;
+
+    for(i=0;i<filteredrows.length;i++) {
+        var rowData  = filteredrows[i];
+//        console.log(rowData.Response)
+        if(rowData.Response == 'true') {
+            trueCount++;
+        }
+        else {
+            falseCount++;
+        }
+    }
+    var r=[trueCount,falseCount];
+    return r;
 }
 
 function displayGraph() {
-    nv.addGraph(function () {
+
+    var pieChart = nv.addGraph(function () {
         var width = 300,
             height = 300;
 
@@ -65,7 +131,7 @@ function displayGraph() {
 
         d3.select("#test1")
             .datum(dataarr)
-            .transition().duration(1200)
+            .transition().duration(100)
             .attr('width', width)
             .attr('height', height)
             .call(chart);
@@ -85,13 +151,13 @@ function pieData(key, y) {
 
 var dataarr = [];
 
-function generatePieData() {
-    var pd1 = new pieData("True", 14);
-    var pd2 = new pieData("False", 19);
+function generatePie(trueCount,falseCount) {
+
+    var pd1 = new pieData("True", trueCount);
+    var pd2 = new pieData("False", falseCount);
     dataarr.push(pd1);
     dataarr.push(pd2);
 
-    console.log(dataarr[0]);
     displayGraph();
 }
 
